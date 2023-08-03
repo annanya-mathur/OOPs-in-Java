726b7d35e2f0f4608d8f8111fd4e73d10f2f33b4
Annanya Mathur <68476475+annanya-mathur@users.noreply.github.com>
2023-08-03 18:51:19 +0530

    Initial Commit

---
+++ class.java
@@ -0,0 +1,20 @@
+class pen{
+    
+    String color;
+    String type;
+    public void write()
+    {
+        System.out.println("Writting");
+        System.out.println(type);
+        System.out.println(color);
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	     pen p1= new pen();
+	     p1.color="blue";
+	     p1.type="ball";
+	     p1.write();
+	}
+}
